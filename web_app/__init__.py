5d85f9344e066c851852aaa097331226aeb14caa
= <=>
2021-11-05 23:43:48 +0300

    update init.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 login = LoginManager(app)
 login.login_view = 'login'
 
+# must be here
 from web_app import routes
 
 if __name__ == '__main__':
